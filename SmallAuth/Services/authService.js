bffaf5ca5964c457e931e4621db25258f35a2070
Setredid <bogbar05@gmail.com>
2022-02-07 16:09:58 +0200

    finish

--- SmallAuth/Services/authService.js
+++ SmallAuth/Services/authService.js
@@ -4,7 +4,7 @@ class AuthService {
     /**
      * The user was supposed to specify the journal to change, and then be able to perform all functions without
      * entering the journal name, using the token. Didn't finish the thing due to the lack of time.
-     * @param {*} data 
+     * @param {String} journalName 
      * @returns 
      */
     async login(journalName) {
