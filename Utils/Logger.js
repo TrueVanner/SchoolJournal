73de605fb5db65416f0eef2235014da02b53c313
TrueVanner <93916208+TrueVanner@users.noreply.github.com>
2022-03-12 15:38:56 +0200

    Add files via upload

--- Utils/Logger.js
+++ Utils/Logger.js
@@ -2,91 +2,81 @@
 // Logger.<TYPE OF ERROR>(<PARAMS>);
 
 // Error builder
-// <TYME> | <TYPE> | <PARAMS>
+// <TIME> | <TYPE> | <PARAMS>
 
 // Logger example
 // 22-01-2022 23:48:30 | ERROR | Failed connection to DataBase
 
-class Logger{
-    // ERROR is an unexpected event and could impact the functionality of your app
-    static error(infoAboutError){
-        const now = new Date;
-        let date = now.getDate();
-        let month = now.getMonth() + 1;
-        let year = now.getFullYear();
+// Get Date of logger output
+function getDate() {
+	const now = new Date;
+	let day = now.getDate();
+	if(day < 10)
+	day = "0" + day;
+	let month = now.getMonth() + 1;
+	if(month < 10)
+		month = "0" + month;
+	let year = now.getFullYear();
 
-        let hour = now.getHours();
-        let minutes = now.getMinutes();
-        let seconds = now.getSeconds();
+	let hour = now.getHours();
+	if(hour < 10)
+		hour = "0" + hour;
+	let minutes = now.getMinutes();
+	if(minutes < 10)
+		minutes = "0" + minutes;
+	let seconds = now.getSeconds();
+	if(seconds < 10)
+		seconds = "0" + seconds;
 
-        console.log(`\n\x1b[31m${date}-${month}-${year} ${hour}:${minutes}:${seconds} | ERROR | ${infoAboutError}\n\x1b[0m`);
-    }
+	let date = day + "-" + month + "-" + year + " " + hour + ":" + minutes + ":" + seconds;
+	
+	return date;
+}
 
-    // WARN includes logs that may become errors and that you detected unusual and unexpected app behavior
-    static warn(infoAboutWarn){
-        const now = new Date;
-        let date = now.getDate();
-        let month = now.getMonth() + 1;
-        let year = now.getFullYear();
+class Logger{    
+	// ERROR is an unexpected event and could impact the functionality of your app
+	static error(infoAboutError){
+		let date = getDate();
 
-        let hour = now.getHours();
-        let minutes = now.getMinutes();
-        let seconds = now.getSeconds();
+		console.log(`\n\x1b[31m${date} | ERROR | ${infoAboutError}\n\x1b[0m`);
+	}
 
-        console.log(`\n\x1b[33m${date}-${month}-${year} ${hour}:${minutes}:${seconds} | WARN | ${infoAboutWarn}\n\x1b[0m`);
-    }
+	// WARN includes logs that may become errors and that you detected unusual and unexpected app behavior
+	static warn(infoAboutWarn){
+		let date = getDate();
 
-    // INFO logs reflect normal application behavior
-    static info(infoAboutInfo){
-        const now = new Date;
-        let date = now.getDate();
-        let month = now.getMonth() + 1;
-        let year = now.getFullYear();
+		console.log(`\n\x1b[33m${date} | WARN | ${infoAboutWarn}\n\x1b[0m`);
+	}
 
-        let hour = now.getHours();
-        let minutes = now.getMinutes();
-        let seconds = now.getSeconds();
+	// INFO logs reflect normal application behavior
+	static info(infoAboutInfo){
+		let date = getDate();
 
-        console.log(`\n\x1b[34m${date}-${month}-${year} ${hour}:${minutes}:${seconds} | INFO | ${infoAboutInfo}\n\x1b[0m`);
+		console.log(`\n\x1b[34m${date} | INFO | ${infoAboutInfo}\n\x1b[0m`);
+	} 
 
-    } 
+	// DEBUG logs are particularly useful during debugging
+	static debug(infoAboutDebug){
+		let date = getDate();
 
-    // DEBUG logs are particularly useful during debugging
-    static debug(infoAboutDebug){
-        const now = new Date;
-        let date = now.getDate();
-        let month = now.getMonth() + 1;
-        let year = now.getFullYear();
+		console.log(`\n\x1b[37m${date} | DEBUG | ${infoAboutDebug}\n\x1b[0m`);
+	}
 
-        let hour = now.getHours();
-        let minutes = now.getMinutes();
-        let seconds = now.getSeconds();
+	// SERVER log that shows 
+	static serverStart(PORT){
+		let date = getDate();
 
-        console.log(`\n\x1b[37m${date}-${month}-${year} ${hour}:${minutes}:${seconds} | DEBUG | ${infoAboutDebug}\n\x1b[0m`);
-    }
+		console.log(`\n\x1b[36m${date} | SERVER | PORT:${PORT}\n\x1b[0m`);
+	}
 
-    // SERVER log that shows 
-    static serverStart(PORT){
-        const now = new Date;
-        let date = now.getDate();
-        let month = now.getMonth() + 1;
-        let year = now.getFullYear();
-
-        let hour = now.getHours();
-        let minutes = now.getMinutes();
-        let seconds = now.getSeconds();
-
-        console.log(`\n\x1b[36m${date}-${month}-${year} ${hour}:${minutes}:${seconds} | SERVER | PORT:${PORT}\n\x1b[0m`);
-    }
-
-    // TEST 5 types of logs
-    static tester(){
-        Logger.error("Failed connection to DataBase");
-        Logger.warn("Connection to DataBase is too long");
-        Logger.info("New admin added");
-        Logger.debug("Code works up to this place");
-        Logger.serverStart(3000);
-    }
+	// TEST 5 types of logs
+	static tester(){
+		Logger.error("Failed connection to DataBase");
+		Logger.warn("Connection to DataBase is too long");
+		Logger.info("New admin added");
+		Logger.debug("Code works up to this place");
+		Logger.serverStart(3000);
+	}
 }
 
 // Export 'Logger' to project   
